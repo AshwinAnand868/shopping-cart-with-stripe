61c18597246fe3a2dbc0591dc8e607c0e11d7d92
AshwinAnand868 <anandashwin868@gmail.com>
2024-03-29 18:18:38 -0400

    Add basic functionalities of Product Card and Add to Cart components

---
+++ nextjs-stripe/data/products.ts
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        id: 'price_1McB1XC4V4bOyW0mEbCcuNPe',
+        name: 'Earthen Bottle',
+        price: 1000,
+        currency: 'USD',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+        imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+    },
+    {
+        id: 'price_1McBPVC4V4bOyW0mqtp8qNLn',
+        name: 'Nomad Tumbler',
+        price: 800,
+        currency: 'USD',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+        imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
+    },
+    {
+        id: 'price_1McBQBC4V4bOyW0m4sCYXwDz',
+        name: 'Focus Paper Refill',
+        price: 650,
+        currency: 'USD',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+    },
+    {
+        id: 'price_1McBQkC4V4bOyW0mk4NGp6bY',
+        name: 'Machined Mechanical Pencil',
+        price: 1320,
+        currency: 'USD',
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+    },
+]
---
+++ nextjs-stripe/src/types/type.ts
@@ -0,0 +1,9 @@
+export type Product = {
+    id: string;
+    name: string;
+    price: number;
+    currency: string;
+    imageSrc: string;
+    imageAlt: string;
+    href: string;
+}
